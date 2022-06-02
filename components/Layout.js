14cfd41a50c0ce41606e70ac485e6df2347545d8
Hayden Johnson <hjohnson@xtivia.com>
2022-06-02 10:07:27 -0600

    up to date commit

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,7 @@ export function Layout ({landingData}) {
         if (section.sys.contentType.sys.id == "heroImage") return <HeroImage key={index} heroData={section.fields}/>
         if (section.sys.contentType.sys.id == "setOfCard") return <CardsSection key={index} cardData={section.fields} style={section.fields.cards[0].fields.materialDesignIcon ? "icon" : "image"}/>
         if (section.sys.contentType.sys.id == "card") return <SectionCard key={index} section={section.fields}/>
-        if (section.sys.contentType.sys.id == "fancyCardScroller") return <FancyCardScroller key={index} scrollerData={section.fields}/>
+        // if (section.sys.contentType.sys.id == "fancyCardScroller") return <FancyCardScroller key={index} scrollerData={section.fields}/>
         // if (section.sys.contentType.sys.id == "fancyCardScroller") return <FancyCardScrollerGreen key={index} scrollerData={section.fields}/>
     })
 
