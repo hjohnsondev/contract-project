f045303231b0f4a42afbd0ca049004a1b39478ae
Hayden Johnson <hjohnson@xtivia.com>
2022-06-06 13:26:44 -0600

    changed config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+	env: {
+    CONTENTFUL_PREVIEW_SECRET: process.env.CONTENTFUL_PREVIEW_SECRET,
+		NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+		CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID
+	}
 }
 
 module.exports = nextConfig
