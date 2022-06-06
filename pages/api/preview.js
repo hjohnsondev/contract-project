a7bab13fed7d2ebfe2d9a2de50e42e8528086bb5
Hayden Johnson <hjohnson@xtivia.com>
2022-06-06 13:10:57 -0600

    logic in preview route

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -3,12 +3,12 @@ import { getPreviewLandingBySlug } from "../../utils/api"
 export default async function handler(req, res) {
   const { secret, slug } = req.query
 
-  /*
+  const isMatch = secret !== process.env.CONTENTFUL_PREVIEW_SECRET ? false : true;
+
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET || !slug) {
-    return res.status(401).json({ message: 'Invalid token', secret: secret, toMatch: process.env.CONTENTFUL_PREVIEW_SECRET, slug: slug });
+    return res.status(401).json({ message: 'Invalid token', secret: secret, isMatch, slug: slug });
   }
   
-
   // Fetch the headless CMS to check if the provided `slug` exists
   const page = await getPreviewLandingBySlug(slug)
 
@@ -16,7 +16,6 @@ export default async function handler(req, res) {
   if (!page.fields.sections.length) {
     return res.status(401).json({ message: 'Invalid slug' })
   }
-  */
 
   // Enable Preview Mode by setting the cookies
   res.setPreviewData({})
