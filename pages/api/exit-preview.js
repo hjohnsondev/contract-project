e98f6adec0726c0207549431584b9007305dfaa2
Hayden Johnson <hjohnson@xtivia.com>
2022-06-07 15:50:53 -0600

    added exit preview mode to the same page being previewed

--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,8 +1,18 @@
-export default function handler(_, res) {
+export default function handler(req, res) {
+  const { url } = req;
+
   // Exit the current user from "Preview Mode". This function accepts no args.
   res.clearPreviewData()
 
-  // Redirect the user back to the index page.
-  res.writeHead(307, { Location: '/' })
+  const route = url.split("=")[1];
+
+  //Redirect the user back to the page they're previewing.
+  res.setHeader('Content-Type', 'text/html')
+  res.write(
+    `<!DOCTYPE html><html><head><meta http-equiv="Refresh" content="0; url=${route}" />
+    <script>window.location.href = '${route}'</script>
+    </head>
+    </html>`
+  )
   res.end()
 }
